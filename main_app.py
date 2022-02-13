ad4b78af4e04d619a6d373802ab4207a3bacea2f
es-ethansiao <16128@my.sanctamaria.school.nz>
2022-02-14 09:55:31 +1300

    Lesson 6 - Flask Blog 4
    Added posts to main python app using a list of dictionaries and then used parameter to loop data into HTML app

--- main_app.py
+++ main_app.py
@@ -1,11 +1,43 @@
 from flask import Flask, render_template
 
+posts = [
+    
+    {
+        'author': 'Kimi Realon',
+        'title': 'Blog Post 1',
+        'content': 'This is my first post on this sh!tty blog app :D. HAHAHA I posted before @Ethan Siao',
+        'date_posted': 'February 13, 2022'        
+    },
+    
+    {
+        'author': 'Ethan Siao',
+        'title': 'Blog Post 2',
+        'content': 'This is my first post on this sh!tty blog app :D F*ck you @Kimi Realon',
+        'date_posted': 'February 14, 2022'        
+    },
+    
+    {
+        'author': 'Micro Ramos',
+        'title': 'Blog Post 3',
+        'content': 'This is my first post on this sh!tty blog app :D. NIGGER NIGGER NIGGER NIGGERS',
+        'date_posted': 'February 14, 2022'        
+    },
+    
+    {
+        'author': 'Milan Taylor',
+        'title': 'Blog Post 4',
+        'content': 'This is my first post on this sh!tty blog app :D. fuck bitches get money',
+        'date_posted': 'February 14, 2022'        
+    },
+    
+]
+
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
