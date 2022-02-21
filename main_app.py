225204bb949d5b27a6089ff0f5854c91e5d8ad78
es-ethansiao <16128@my.sanctamaria.school.nz>
2022-02-21 22:51:09 +1300

    Flask Blog 10
    Created routes for registration and login forms in the main python app. However, it lacks html files for both registration and login pages.

--- main_app.py
+++ main_app.py
@@ -1,4 +1,14 @@
+# ==================== BLOGGING APP allows users to view and post blogs with an account ====================
+
+# Importing templates from flask installed on device
 from flask import Flask, render_template, url_for
+
+# Importing forms from forms python file
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fe8232dd0a2b9a7760b5978790a9884e'
+
 
 # ==================== LISTS AND DICTIONARIES OF BLOG POSTS ====================
 # Blog posts send titles, authors, content, and date posted to homepage
@@ -46,6 +56,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About') # sends in parameter for title of the webpage
+
+# Route for registration page
+@app.route("/register")
+def register():
+    form = RegistrationForm() # sets form to registration form from the forms python file
+    return render_template('register.html', title='Register', form=form) # form=form creates a form which is set as above
+
+# Route for login page
+@app.route("/login")
+def login():
+    form = LoginForm() # sets form to login form from the forms python file
+    return render_template('login.html', title='Login', form=form) # form=form creates a form which is set as above
 
 # Allows main app to run the web server
 if __name__=='__main__':
