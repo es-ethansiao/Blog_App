3a278df793d8a99a8158be5892d2ad3ee8430789
es-ethansiao <16128@my.sanctamaria.school.nz>
2022-03-07 17:57:12 +1300

    Flask Blog 20
    Added validators to check for duplicate usernames and emails in the database before successfully registering account

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from main_app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,8 +10,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
     
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first() # queries database to check if there is already a username in the database
+        if True:
+            raise ValidationError('That username is already taken! Please choose another one.')
     
-    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first() # queries database to check if there is already an email in the database
+        if True:
+            raise ValidationError('That email is already taken! Please choose another one.')
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
